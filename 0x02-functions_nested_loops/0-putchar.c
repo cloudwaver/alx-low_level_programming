df6b7e1de9b09f8b5852f6f7e1c5f47d6280f7e9
cloudwaver <dejidankuwo@gmail.com>
2022-07-06 14:17:30 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
-#include "putchar.h"
+#include "main.h"
 
 /**
-* main -  prints putchar, followed by a new line
+* main - Entry point
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char *sh = "putchar";
+	
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
 	_putchar('\n');
-
+	
 	return (0);
-
 }
