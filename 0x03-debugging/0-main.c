7021c081398431ce1b38ad85a4a2f033cbb8d7d7
cloudwaver <dejidankuwo@gmail.com>
2022-07-15 13:19:06 -0700

    commit

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,11 @@
-#include <stdlib.h>                                 
-#include <time.h>                                   
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
 
 
-void positive_or_negative(int i){
+void positive_or_negative(int i)
+{
 	int n;
 
 srand(time(0));
@@ -12,8 +13,10 @@ n = i;
 if (n > 0)
 	printf("%d is positive\n", n);
 else if (n == 0)
-	printf("%d is zero\n", n);                  else if (n < 0)
+	printf("%d is zero\n", n);
+else if (n < 0)
 	printf("%d is negative\n", n);
+
 return (0);
 }
 /**
